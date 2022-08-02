4103af6a8ea71c8a240f56c5f96f169f1b33628b
Helaman Del Castillo <helaman.delcastillo@praemium.com>
2022-08-03 00:18:33 +1000

    Add project files.

---
+++ TestProject.Excel/EditSpreadSheet.cs
@@ -0,0 +1,37 @@
+﻿using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject.Excel
+{
+    internal class EditSpreadSheet
+    {
+        public void Edit()
+        {
+            string pathSource = @"C:\temp\test.xlsx";
+
+            IWorkbook templateWorkbook;
+            using (FileStream fs = new FileStream(pathSource, FileMode.Open, FileAccess.Read))
+            {
+                templateWorkbook = new XSSFWorkbook(fs);
+            }
+
+            string sheetName = "ImportTemplate";
+            ISheet sheet = templateWorkbook.GetSheet(sheetName) ?? templateWorkbook.CreateSheet(sheetName);
+            IRow dataRow = sheet.GetRow(4) ?? sheet.CreateRow(4);
+            ICell cell = dataRow.GetCell(1) ?? dataRow.CreateCell(1);
+            cell.SetCellValue("foo");
+
+            using (FileStream fs = new FileStream(pathSource, FileMode.Create, FileAccess.Write))
+            {
+                templateWorkbook.Write(fs);
+
+            }
+        }
+    }
+}
---
+++ TestProject.Excel/Program.cs
@@ -0,0 +1,42 @@
+﻿using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject.Excel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var EditNow = new EditSpreadSheet();
+
+            EditNow.Edit();
+
+
+
+                //IWorkbook workbook = new XSSFWorkbook();
+                //ISheet worksheet = workbook.CreateSheet();
+                //IRow row = worksheet.CreateRow(0);
+                //ICell cell = row.CreateCell(2);
+                //cell.SetCellValue("HelloWorld");
+                //using (var fileData = new FileStream(@"C:\temp\test.xlsx", FileMode.Create))
+                //{
+                //    workbook.Write(fileData);
+                //}
+            
+            
+
+            
+                
+            
+        }
+
+           
+    }
+}
